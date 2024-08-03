f9b6c7c81cff0169cf8bac8a443955ae55597b4c
AR123456 <coloradoz99@gmail.com>
2024-08-03 08:07:14 -0400

    poppins font

--- Daily-UI-27 Dropdown/app.js
+++ Daily-UI-27 Dropdown/app.js
@@ -1,8 +1,14 @@
 // toggle
-const expanded = document.querySelector(".nav-menu-expanded");
-const collapsed = document.querySelector(".nav-collapsed");
+// const expanded = document.querySelector(".nav-menu-expanded");
+// const collapsed = document.querySelector(".nav-collapsed");
 
-collapsed.addEventListener("click", () => {
-  collapsed.classList.toggle("active");
-  expanded.classList.toggle("active");
+// collapsed.addEventListener("click", () => {
+//   collapsed.classList.toggle("active");
+//   expanded.classList.toggle("active");
+// });
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
 });
