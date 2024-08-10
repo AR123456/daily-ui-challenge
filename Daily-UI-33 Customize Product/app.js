77bae884f209f5ba7a853d40fcbc74dd8709f736
AR123456 <coloradoz99@gmail.com>
2024-08-10 08:06:28 -0400

    color picker

--- Daily-UI-33 Customize Product/app.js
+++ Daily-UI-33 Customize Product/app.js
@@ -1,2 +1,8 @@
 const pineapple = document.querySelector(".pineapple");
 const input = document.querySelector("#colorpicker");
+
+input.addEventListener("input", changeColor);
+
+function changeColor() {
+  pineapple.setAttribute("fill", input.value);
+}
