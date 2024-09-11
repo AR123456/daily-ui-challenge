9cf9758206e350e6af583679b1a5855a45cbf631
AR123456 <coloradoz99@gmail.com>
2024-09-11 08:23:41 -0400

    better footer color change

--- Daily-UI-51-Press-page/app.js
+++ Daily-UI-51-Press-page/app.js
@@ -2,9 +2,10 @@ window.addEventListener("scroll", function () {
   console.log(window.scrollY);
   const footer = document.querySelector(".footer");
   if (window.scrollY > 10) {
-    footer.style.backgroundColor = "#bcb8b5";
+    footer.style.backgroundColor = "rgba(187, 183, 180, 0.9)";
+
     console.log(window.scrollY);
   } else {
-    footer.style.backgroundColor = "transparent";
+    footer.style.backgroundColor = "#bcb8b5";
   }
 });
