44cceb75887dbf0a180a8be17aa1f425b775a98d
AR123456 <coloradoz99@gmail.com>
2024-09-12 07:49:38 -0400

    adding some js to change color with ease

--- Daily-UI-52 Logo Design/app.js
+++ Daily-UI-52 Logo Design/app.js
@@ -1,1 +1,12 @@
 // changing colors
+const start = document.getElementById("start");
+const up = document.getElementById("up");
+const startUp = document.getElementById("start-up");
+
+start.addEventListener("click", function () {
+  console.log("you clicked the start svg");
+  start.setAttribute("fill", "red");
+  up.setAttribute("fill", "blue");
+
+  startUp.style.fill = "purple";
+});
