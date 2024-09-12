16e7e0554a91ece70ccc6fc4bd377250e8c3cc9d
AR123456 <coloradoz99@gmail.com>
2024-09-12 07:50:21 -0400

    improving the js

--- Daily-UI-52 Logo Design/app.js
+++ Daily-UI-52 Logo Design/app.js
@@ -4,9 +4,31 @@ const up = document.getElementById("up");
 const startUp = document.getElementById("start-up");
 
 start.addEventListener("click", function () {
-  console.log("you clicked the start svg");
-  start.setAttribute("fill", "red");
-  up.setAttribute("fill", "blue");
+  //   console.log("you clicked the start svg");
+  //   start.setAttribute("fill", "red");
+  //   up.setAttribute("fill", "blue");
+  //   startUp.style.fill = "purple";
+  // Initially hide all SVGs
+  start.style.display = "none";
+  up.style.display = "none";
+  startUp.style.display = "none";
 
-  startUp.style.fill = "purple";
+  // Show the first SVG after 0.5s
+  setTimeout(() => {
+    start.style.display = "block";
+    start.setAttribute("fill", "red");
+  }, 500);
+
+  // Show the second SVG after 2s
+  setTimeout(() => {
+    start.style.display = "none";
+    uo.style.display = "block";
+    up.setAttribute("fill", "blue");
+  }, 2000);
+
+  // Hide both and show the third SVG after 4s
+  setTimeout(() => {
+    document.getElementById("up").style.display = "none";
+    document.getElementById("start-up").style.display = "block";
+  }, 4000);
 });
