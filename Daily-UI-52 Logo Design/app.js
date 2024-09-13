25ac016d92de223d2d3021bc5a4ba321e9f1ec7e
AR123456 <coloradoz99@gmail.com>
2024-09-13 07:38:59 -0400

    have css size and js hiding revealing

--- Daily-UI-52 Logo Design/app.js
+++ Daily-UI-52 Logo Design/app.js
@@ -4,10 +4,6 @@ const up = document.getElementById("up");
 const startUp = document.getElementById("start-up");
 
 start.addEventListener("click", function () {
-  //   console.log("you clicked the start svg");
-  //   start.setAttribute("fill", "red");
-  //   up.setAttribute("fill", "blue");
-  //   startUp.style.fill = "purple";
   // Initially hide all SVGs
   start.style.display = "none";
   up.style.display = "none";
@@ -22,13 +18,14 @@ start.addEventListener("click", function () {
   // Show the second SVG after 2s
   setTimeout(() => {
     start.style.display = "none";
-    uo.style.display = "block";
+    up.style.display = "block";
     up.setAttribute("fill", "blue");
   }, 2000);
 
   // Hide both and show the third SVG after 4s
   setTimeout(() => {
-    document.getElementById("up").style.display = "none";
-    document.getElementById("start-up").style.display = "block";
+    up.style.display = "none";
+    startUp.style.display = "block";
+    startUp.style.fill = "purple";
   }, 4000);
 });
