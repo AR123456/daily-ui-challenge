2a23d1eaad6cbb7ef60f0333605faf5346a90378
AR123456 <coloradoz99@gmail.com>
2024-09-26 07:55:37 -0400

    forward and back

--- Daily-UI-57 Video Player/app.js
+++ Daily-UI-57 Video Player/app.js
@@ -7,6 +7,8 @@ const fwd = document.querySelector(".fwd");
 const timerWrapper = document.querySelector(".timer");
 const timer = document.querySelector(".timer span");
 const timerBar = document.querySelector(".timer div");
+let intervalFwd;
+let intervalRwd;
 
 media.removeAttribute("controls");
 controls.style.visibility = "visible";
@@ -18,5 +20,44 @@ function playPauseMedia() {
   } else {
     play.setAttribute("data-icon", "P");
     media.pause();
+  }
+}
+stop.addEventListener("click", stopMedia);
+media.addEventListener("ended", stopMedia);
+function stopMedia() {
+  media.pause();
+  media.currentTime = 0;
+  play.setAttribute("data-icon", "P");
+}
+rwd.addEventListener("click", mediaBackward);
+fwd.addEventListener("click", mediaForward);
+
+function mediaBackward() {
+  clearInterval(intervalFwd);
+  fwd.classList.remove("active");
+
+  if (rwd.classList.contains("active")) {
+    rwd.classList.remove("active");
+    clearInterval(intervalRwd);
+    media.play();
+  } else {
+    rwd.classList.add("active");
+    media.pause();
+    intervalRwd = setInterval(windBackward, 200);
+  }
+}
+
+function mediaForward() {
+  clearInterval(intervalRwd);
+  rwd.classList.remove("active");
+
+  if (fwd.classList.contains("active")) {
+    fwd.classList.remove("active");
+    clearInterval(intervalFwd);
+    media.play();
+  } else {
+    fwd.classList.add("active");
+    media.pause();
+    intervalFwd = setInterval(windForward, 200);
   }
 }
