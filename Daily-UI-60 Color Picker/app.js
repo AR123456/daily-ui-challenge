1e0b4ea7aacda4ae02beee93638f068d91b4fa60
AR123456 <coloradoz99@gmail.com>
2024-09-30 07:30:11 -0400

    added event listener

---
+++ Daily-UI-60 Color Picker/app.js
@@ -0,0 +1,45 @@
+const colorPicker = document.getElementById("color");
+const opacityPicker = document.getElementById("opacity");
+
+document.addEventListener("readystatechange", () => {
+  // The show() method of the HTMLDialogElement interface displays the dialog modelessly, i.e. still allowing interaction with content outside of the dialog.
+  document.querySelector("dialog").show();
+  createColor();
+});
+colorPicker.addEventListener("change", () => {
+  createColor();
+});
+opacityPicker.addEventListener("change", () => {
+  createColor();
+});
+
+function createColor() {
+  const currentColor = colorPicker.value;
+  const currentOpacity = Number(opacityPicker.value);
+  setBackgroundColor(currentColor, currentOpacity);
+  createHex(currentColor, currentOpacity);
+  createRGB(currentColor, currentOpacity);
+}
+
+function createHex(color, opacity) {
+  console.log(color, opacity);
+}
+function createRGB(color, opacity) {
+  console.log(color, opacity);
+}
+
+function createColorFunc(r, g, b, opacity) {
+  console.log(r, g, b, opacity);
+}
+function createHSL(r, g, b, opacity) {
+  console.log(r, g, b, opacity);
+}
+function createHWB(h, s, l, opacity) {
+  console.log(h, s, l, opacity);
+}
+function setBackgroundColor(color, opacity) {
+  console.log(color, opacity);
+}
+function hexOpacity(color, opacity) {
+  console.log(color, opacity);
+}
