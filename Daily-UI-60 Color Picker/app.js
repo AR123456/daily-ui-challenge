b57dd2fc4ab2c9808d7ff335908bbdb4dd5f3863
AR123456 <coloradoz99@gmail.com>
2024-10-02 07:37:02 -0400

    hsl work

--- Daily-UI-60 Color Picker/app.js
+++ Daily-UI-60 Color Picker/app.js
@@ -72,19 +72,30 @@ function createRGB(color, opacity) {
 // to create sRGB is a standard RGB (red, green, blue) color space
 function createColorFunc(r, g, b, opacity) {
   const cell = document.querySelector("#colorfunc td");
-  console.log(r, g, b, opacity);
-  const R = Number(r / 255).toFixed(2);
-  const G = Number(g / 255).toFixed(2);
-  const B = Number(b / 255).toFixed(2);
+
+  const R = Number((r / 255).toFixed(2));
+  const G = Number((g / 255).toFixed(2));
+  const B = Number((b / 255).toFixed(2));
   if (opacity === 1) {
     cell.textContent = `color(srgb ${R} ${G} ${B})`;
   } else {
     cell.textContent = `color(srgb ${R} ${G} ${B} / ${opacity})`;
   }
-  console.log(`color(srgb ${R} ${G} ${B} / ${opacity})`);
+  // console.log(`color(srgb ${R} ${G} ${B} / ${opacity})`);
 }
 function createHSL(r, g, b, opacity) {
-  // console.log(r, g, b, opacity);
+  const cell = document.querySelector("#HSL td");
+  console.log(r, g, b, opacity);
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+  //  get min , max and diff or delta
+  const cmin = Math.min(r, g, b);
+  const cmax = Math.max(r, g, b);
+  const delta = cmax - cmin;
+  let h = 0;
+  let s = 0;
+  let l = 0;
 }
 function createHWB(h, s, l, opacity) {
   // console.log(h, s, l, opacity);
