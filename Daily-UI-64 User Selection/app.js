e8c2800d0e7487e4fc0cc1702b2dbb83858a8385
AR123456 <coloradoz99@gmail.com>
2024-10-13 08:48:19 -0400

    another example and code fix

--- Daily-UI-64 User Selection/app.js
+++ Daily-UI-64 User Selection/app.js
@@ -1,18 +1,13 @@
-const els = (sel, par = document) => par.querySelectorAll(sel);
-const el = (sel, par = document) => par.querySelector(sel);
-const mod = (n, m) => ((n % m) + m) % m;
+let carousel = document.querySelector(".carousel");
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let item = document.querySelector(".item");
 
-els(".slider-wrapper").forEach((elPar) => {
-  const elSlider = el(".slider", elPar);
-  const elsItems = els(".item", elPar);
-  const sub = +elPar.dataset.items ?? 1;
-  const tot = Math.ceil(elsItems.length / sub);
-  let c = 0;
-
-  const anim = () => (elSlider.style.transform = `translateX(-${c * 100}%)`);
-  const prev = () => ((c = mod(c - 1, tot)), anim());
-  const next = () => ((c = mod(c + 1, tot)), anim());
-
-  el(".prev", elPar).addEventListener("click", prev);
-  el(".next", elPar).addEventListener("click", next);
-});
+(function () {
+  right.addEventListener("click", function (e) {
+    carousel.scrollLeft += item.clientWidth;
+  });
+  left.addEventListener("click", function () {
+    carousel.scrollLeft -= item.clientWidth;
+  });
+})();
