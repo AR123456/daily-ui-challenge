bd6b9f0ccc7aeac39188a6e57a859eb8c9331058
AR123456 <coloradoz99@gmail.com>
2024-10-26 04:47:32 -0400

    working on the tabs

--- Daily-UI-69-Trending/app.js
+++ Daily-UI-69-Trending/app.js
@@ -3,48 +3,73 @@ const width = 50;
 const height = 50;
 const margin = { top: 5, right: 5, bottom: 5, left: 5 };
 
-// Data for the line chart (simple linear data points)
-const data = [
+// Data for the first line chart
+const data1 = [
   { x: 0, y: 0 },
   { x: 10, y: 15 },
   { x: 20, y: 10 },
   { x: 30, y: 20 },
   { x: 40, y: 25 },
   { x: 50, y: 35 },
+  { x: 60, y: 20 },
+  { x: 70, y: 10 },
+  { x: 80, y: 5 },
 ];
 
-// Create SVG element
-const svg = d3
-  .select("#chart")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// Data for the second line chart
+const data2 = [
+  { x: 0, y: 30 },
+  { x: 10, y: 25 },
+  { x: 20, y: 20 },
+  { x: 30, y: 15 },
+  { x: 40, y: 10 },
+  { x: 50, y: 5 },
+  { x: 60, y: 15 },
+  { x: 70, y: 25 },
+  { x: 80, y: 30 },
+];
 
-// Set up scales
-const xScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, (d) => d.x)])
-  .range([0, width - margin.left - margin.right]);
+// Function to create the graph
+function createGraph(container, data, strokeColor) {
+  // Create SVG element
+  const svg = d3
+    .select(container)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-const yScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, (d) => d.y)])
-  .range([height - margin.top - margin.bottom, 0]);
+  // Set up scales
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.x)])
+    .range([0, width - margin.left - margin.right]);
 
-// Line generator
-const line = d3
-  .line()
-  .x((d) => xScale(d.x))
-  .y((d) => yScale(d.y))
-  .curve(d3.curveMonotoneX); // Smooth the line
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.y)])
+    .range([height - margin.top - margin.bottom, 0]);
 
-// Append path (line)
-svg
-  .append("path")
-  .datum(data)
-  .attr("d", line)
-  .attr("fill", "none")
-  .attr("stroke", "blue")
-  .attr("stroke-width", 1);
+  // Line generator
+  const line = d3
+    .line()
+    .x((d) => xScale(d.x))
+    .y((d) => yScale(d.y))
+    .curve(d3.curveMonotoneX);
+
+  // Append path (line)
+  svg
+    .append("path")
+    .datum(data)
+    .attr("d", line)
+    .attr("fill", "none")
+    .attr("stroke", strokeColor)
+    .attr("stroke-width", 1);
+}
+
+// Create the first graph (with blue line)
+createGraph("#chart1", data1, "blue");
+
+// Create the second graph (with red line)
+createGraph("#chart2", data2, "red");
