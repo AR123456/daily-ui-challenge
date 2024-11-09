924c17b1ee0eabd2de57f77fbb9c2452cfa08f7c
AR123456 <coloradoz99@gmail.com>
2024-11-09 09:27:55 -0500

    carousel slider

--- Daily-UI-72-Carousel Slider/app.js
+++ Daily-UI-72-Carousel Slider/app.js
@@ -8,3 +8,15 @@ function prevSlide() {
   if (index < 0) index = slides.length - 1;
   slides[index].classList.add("active");
 }
+document.querySelector(".prev").addEventListener("click", (e) => {
+  prevSlide();
+});
+function nextSlide() {
+  slides[index].classList.remove("active");
+  index++;
+  if (index > slides.length - 1) index = 0;
+  slides[index].classList.add("active");
+}
+document.querySelector(".next").addEventListener("click", (e) => {
+  nextSlide();
+});
