4adb373f2846e7362750ee80d157b723a5c8c952
AR123456 <coloradoz99@gmail.com>
2024-11-10 08:04:51 -0500

    adding js

--- Daily-UI-72-Carousel Slider/app.js
+++ Daily-UI-72-Carousel Slider/app.js
@@ -1,1 +1,31 @@
-  
+//https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  let i;
+
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  console.log(slides, dots);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].computedStyleMap.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].computedStyleMap.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
