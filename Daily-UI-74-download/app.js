7976cb7baf8f562788a33aa797fdf6e731445b60
AR123456 <coloradoz99@gmail.com>
2024-11-15 08:42:23 -0500

    logging click

---
+++ Daily-UI-74-download/app.js
@@ -0,0 +1,7 @@
+const download = document.querySelector(".download");
+const upload = document.querySelector(".upload");
+
+const toggler = () => {
+  console.log("clicked");
+};
+download.addEventListener("click", toggler);
