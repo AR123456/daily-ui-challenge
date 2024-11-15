b0a6ebadd09ef32f0149a940a27c53a206346491
AR123456 <coloradoz99@gmail.com>
2024-11-15 08:43:01 -0500

    looking at js

--- Daily-UI-74-download/app.js
+++ Daily-UI-74-download/app.js
@@ -1,7 +1,1 @@
-const download = document.querySelector(".download");
-const upload = document.querySelector(".upload");
 
-const toggler = () => {
-  console.log("clicked");
-};
-download.addEventListener("click", toggler);
