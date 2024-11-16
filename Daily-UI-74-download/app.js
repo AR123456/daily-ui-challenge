f7a230f87eaef3a2036ba14547280cf117782761
AR123456 <coloradoz99@gmail.com>
2024-11-16 10:15:56 -0500

    downloading on click

--- Daily-UI-74-download/app.js
+++ Daily-UI-74-download/app.js
@@ -1,1 +1,13 @@
+const download = document.querySelector(".download");
+const upload = document.querySelector(".upload");
 
+const changeDL = () => {
+  download.style.background =
+    "url('./downloading_24.svg') center/cover no-repeat";
+};
+const changeUL = () => {
+  upload.style.background =
+    "url('./arrow_upload_progress24.svg') center/cover no-repeat";
+};
+download.addEventListener("click", changeDL);
+upload.addEventListener("click", changeUL);
