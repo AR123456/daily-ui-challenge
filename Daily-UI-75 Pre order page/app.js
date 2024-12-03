f1fac2f568168486fa6e04bec348fa60879bd545
AR123456 <coloradoz99@gmail.com>
2024-12-03 09:17:15 -0500

    using css filter to change svg color on hover

--- Daily-UI-75 Pre order page/app.js
+++ Daily-UI-75 Pre order page/app.js
@@ -1,8 +1,1 @@
-const social = document.querySelectorAll(".soc-link");
-
-for (let i = 0; i < social.length; i++) {
-  social[i].addEventListener("mouseover", () => {
-    const svg = social[i].querySelector("img");
-    svg.style.fill = "red";
-  });
-}
+ 
