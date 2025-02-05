c50eb26557afe9dd6b28790b2c9c2ce79ed80a50
AR123456 <coloradoz99@gmail.com>
2025-02-05 08:45:32 -0500

    changing over to css filter

--- Daily-UI-85 Pagination/app.js
+++ Daily-UI-85 Pagination/app.js
@@ -1,37 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var dots = document.querySelectorAll(".dot");
-//   var select = document.querySelector(".select");
-
-//   dots.forEach(function (dot) {
-//     dot.addEventListener("mouseover", function () {
-//       var dest = dot.offsetLeft;
-//       var t = 0.6;
-//       TweenMax.to(select, t, { x: dest, ease: Back.easeOut });
-//     });
-//   });
-
-//   var lastPos = select.offsetLeft;
-
-//   function updateScale() {
-//     var pos = select.offsetLeft;
-//     var speed = Math.abs(pos - lastPos);
-//     var d = 44;
-//     var offset = -20;
-//     var hd = d / 2;
-//     var scale = (offset + pos) % d;
-//     if (scale > hd) {
-//       scale = hd - (scale - hd);
-//     }
-//     scale = 1 - (scale / hd) * 0.35;
-//     TweenMax.to(select, 0.1, { scaleY: scale, scaleX: 1 + speed * 0.06 });
-
-//     lastPos = pos;
-//     requestAnimationFrame(updateScale);
-//   }
-
-//   requestAnimationFrame(updateScale);
-//   dots[0].dispatchEvent(new Event("mouseover"));
-// });
 document.addEventListener("DOMContentLoaded", function () {
   var dots = document.querySelectorAll(".dot");
   var select = document.querySelector(".select");
@@ -58,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     scale = 1 - (scale / hd) * 0.35;
     select.style.transition = "transform 0.1s";
-    select.style.transform = `scaleY(${scale}) scaleX(${1 + speed * 0.06})`;
+    select.style.transform += ` scaleY(${scale}) scaleX(${1 + speed * 0.06})`;
 
     lastPos = pos;
     requestAnimationFrame(updateScale);
