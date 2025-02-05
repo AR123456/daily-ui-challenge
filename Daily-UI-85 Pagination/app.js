30e78b57bea33371f655338ae154bb6d30db7c2c
AR123456 <coloradoz99@gmail.com>
2025-02-05 08:42:30 -0500

    gooey source code

--- Daily-UI-85 Pagination/app.js
+++ Daily-UI-85 Pagination/app.js
@@ -1,34 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const select = document.querySelector(".select");
-  const dots = document.querySelectorAll(".dot");
-  let lastPos = 0;
-
-  dots.forEach((dot) => {
-    dot.addEventListener("mouseover", function () {
-      let dest = this.offsetLeft;
-      select.style.transform = `translateX(${dest}px)`;
-    });
+$(document).ready(function () {
+  $(".dot").hover(function () {
+    var cur = $(this);
+    var dest = cur.position().left;
+    var t = 0.6;
+    TweenMax.to($(".select"), t, { x: dest, ease: Back.easeOut });
   });
-
+  var lastPos = $(".select").position().left;
   function updateScale() {
-    let pos = select.getBoundingClientRect().left;
-    let speed = Math.abs(pos - lastPos);
-    let d = 44;
-    let offset = -20;
-    let hd = d / 2;
-    let scale = (offset + pos) % d;
+    var pos = $(".select").position().left;
 
+    var speed = Math.abs(pos - lastPos);
+    var d = 44;
+    var offset = -20;
+    var hd = d / 2;
+    var scale = (offset + pos) % d;
     if (scale > hd) {
       scale = hd - (scale - hd);
     }
     scale = 1 - (scale / hd) * 0.35;
-
-    select.style.transform += ` scale(${1 + speed * 0.06}, ${scale})`;
+    TweenMax.to($(".select"), 0.1, { scaleY: scale, scaleX: 1 + speed * 0.06 });
 
     lastPos = pos;
     requestAnimationFrame(updateScale);
   }
-
   requestAnimationFrame(updateScale);
-  dots[0].dispatchEvent(new Event("mouseover"));
+  $(".dot:eq(0)").trigger("mouseover");
 });
