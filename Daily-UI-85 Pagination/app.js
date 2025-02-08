ba1fc44eae7ecb21a7ce8a0f00a1129a90e0c905
AR123456 <coloradoz99@gmail.com>
2025-02-08 08:11:22 -0500

    handling the prev and next buttons

--- Daily-UI-85 Pagination/app.js
+++ Daily-UI-85 Pagination/app.js
@@ -1,9 +1,32 @@
 const paginationNumbers = document.getElementById("pagination-numbers");
 const paginatedList = document.getElementById("paginated-list");
 const listItems = paginatedList.querySelectorAll("li");
-console.log(typeof listItems, listItems);
+
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
 
 const paginationLimit = 10;
 const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+const handlePageButtonStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
