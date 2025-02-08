662e0f853641d5c0658300012fb0b8363bdda65e
AR123456 <coloradoz99@gmail.com>
2025-02-08 08:10:49 -0500

    start of js declare some vars

--- Daily-UI-85 Pagination/app.js
+++ Daily-UI-85 Pagination/app.js
@@ -1,1 +1,9 @@
- 
+const paginationNumbers = document.getElementById("pagination-numbers");
+const paginatedList = document.getElementById("paginated-list");
+const listItems = paginatedList.querySelectorAll("li");
+console.log(typeof listItems, listItems);
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+const paginationLimit = 10;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
