2460fa4662b824ea1df6465e73e2b9a5cedb98ba
AR123456 <coloradoz99@gmail.com>
2025-02-21 07:33:00 -0500

    adding some javascript

--- Daily-UI-86 Progress/app.js
+++ Daily-UI-86 Progress/app.js
@@ -1,7 +1,9 @@
-// const progressBar = document.getElementsByClassName("progress-bar")[0];
+// getting first ele of this array - why not just get it since there is only one  in this case?
 
-// setInterval(() => {
-//   const computedStyle = getComputedStyle(progressBar);
-//   const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
-//   progressBar.style.setProperty("--width", width + 0.1);
-// }, 5);
+const progressBar = document.getElementsByClassName("progress-bar")[0];
+// call the function within at particular interval
+setInterval(() => {
+  const computedStyle = getComputedStyle(progressBar);
+  const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+  progressBar.style.setProperty("--width", width + 0.1);
+}, 5);
