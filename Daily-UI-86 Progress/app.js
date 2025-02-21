22fbd0c3431adb5b434ca980144314595516b2ea
AR123456 <coloradoz99@gmail.com>
2025-02-21 07:33:41 -0500

    clearing the interval

--- Daily-UI-86 Progress/app.js
+++ Daily-UI-86 Progress/app.js
@@ -1,13 +1,20 @@
 // getting first ele of this array - why not just get it since there is only one  in this case?
-
+// re code to run on page load
 const progressBar = document.getElementsByClassName("progress-bar")[0];
 // call the function within at particular interval
-setInterval(() => {
+let interval = setInterval(() => {
   // get current width of progress bar
   const computedStyle = getComputedStyle(progressBar);
   // increment width
   // get string and make into num with parseFloat
+  // if it does not exist set to 0
   const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+  // now set the width to the calculated width and increment 0.1 every 5 sec
   progressBar.style.setProperty("--width", width + 0.1);
   // every 5 mili sec
 }, 5);
+// Clear interval after 3 seconds
+setTimeout(() => {
+  clearInterval(interval);
+  console.log("Interval cleared after 3 seconds");
+}, 3000);
