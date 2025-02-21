f4c00cb0ca322a1149e94395a1e561949f962d14
AR123456 <coloradoz99@gmail.com>
2025-02-21 07:33:19 -0500

    incrementing width

--- Daily-UI-86 Progress/app.js
+++ Daily-UI-86 Progress/app.js
@@ -3,7 +3,11 @@
 const progressBar = document.getElementsByClassName("progress-bar")[0];
 // call the function within at particular interval
 setInterval(() => {
+  // get current width of progress bar
   const computedStyle = getComputedStyle(progressBar);
+  // increment width
+  // get string and make into num with parseFloat
   const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
   progressBar.style.setProperty("--width", width + 0.1);
+  // every 5 mili sec
 }, 5);
