8cf24beded16ebb4e7c34288c4a064614314665a
AR123456 <coloradoz99@gmail.com>
2025-02-22 04:50:12 -0500

    bar working mouse over

--- Daily-UI-86 Progress/app.js
+++ Daily-UI-86 Progress/app.js
@@ -1,20 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const progressBar = document.querySelector(".progress-bar");
-  let progress = 0;
-  let startTime;
 
-  function animateProgressBar(timestamp) {
-    if (!startTime) startTime = timestamp;
-    const elapsed = timestamp - startTime;
-
-    if (elapsed < 3000) {
-      progress = Math.min(100, elapsed / 30);
-      progressBar.style.setProperty("--width", progress);
-      requestAnimationFrame(animateProgressBar);
-    } else {
-      console.log("Animation completed");
-    }
-  }
-
-  requestAnimationFrame(animateProgressBar);
+  progressBar.addEventListener("mouseenter", () => {
+    progressBar.style.setProperty("--width", "100");
+  });
+  progressBar.addEventListener("mouseleave", () => {
+    progressBar.style.setProperty("--width", "0");
+  });
 });
