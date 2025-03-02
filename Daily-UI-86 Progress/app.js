05937179b4c3726fb827209156b6684e78b9b1cc
AR123456 <coloradoz99@gmail.com>
2025-03-02 05:28:54 -0500

    better grid layout

--- Daily-UI-86 Progress/app.js
+++ Daily-UI-86 Progress/app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const progressBar = document.querySelector(".progress-bar");
   const grid = document.querySelector(".grid");
   const cardTemplate = document.getElementById("card-template");
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 6; i++) {
     grid.append(cardTemplate.content.cloneNode(true));
   }
 
