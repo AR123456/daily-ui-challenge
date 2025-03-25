47524b8a895c8b41d86c618727e5e29cfa1c3b36
AR123456 <coloradoz99@gmail.com>
2025-03-25 05:39:26 -0400

    working on toggle

--- Daily-UI-92 FAQ/app.js
+++ Daily-UI-92 FAQ/app.js
@@ -1,1 +1,6 @@
-//  when the svg is clicked toggle
+const container = document.querySelector(".container");
+const card = document.querySelector(".card");
+container.addEventListener("click", () => {
+  console.log("click");
+  card.classList.toggle("hidden");
+});
