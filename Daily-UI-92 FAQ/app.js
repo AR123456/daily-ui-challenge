d12b576dbb8bbc9525030be0fb1641387bb6d8f9
AR123456 <coloradoz99@gmail.com>
2025-03-29 07:42:38 -0400

    remove contact

--- Daily-UI-92 FAQ/app.js
+++ Daily-UI-92 FAQ/app.js
@@ -1,13 +1,6 @@
 const expand = document.querySelector(".col-expander");
-const contract = document.querySelector(".col-contractor");
-const short = document.querySelector(".short");
-const long = document.querySelector(".long");
-
-expand.addEventListener("click", () => {
-  short.classList.add("hidden");
-  long.classList.remove("hidden");
-});
-contract.addEventListener("click", () => {
-  long.classList.add("hidden");
-  short.classList.remove("hidden");
+const contract = document.querySelector(".contractor");
+container.addEventListener("click", () => {
+  console.log("click");
+  card.classList.add("hidden");
 });
