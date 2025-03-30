2f6a823bfcb0beb87351384f617c5ffb856deffd
AR123456 <coloradoz99@gmail.com>
2025-03-30 08:29:29 -0400

    rotation function

--- Daily-UI-95 Product Tour/app.js
+++ Daily-UI-95 Product Tour/app.js
@@ -12,10 +12,23 @@ const path = d3.geoPath(projection, context);
 let rotation = [0, 0, 23.5];
 let speed = 0.2;
 
-d3.json("https://unpkg.com/world-atlas@2.0.2/countries-110m.json").then(
-  (world) => {
+d3.json("https://unpkg.com/world-atlas@2.0.2/countries-110m.json")
+  .then((world) => {
     const land = topojson.feature(world, world.objects.land);
     const countries = topojson.feature(world, world.objects.countries);
     const graticlue = d3.geoGraticlude10();
-  }
-);
+    function render() {
+      context.clearRect(0, 0, width, height);
+      // draw water background sphere
+    }
+    // rotation function
+    function rotate() {
+      rotation[0] += speed;
+      projection.rotate(rotation);
+      render();
+      requestAnimationFrame(rotate);
+    }
+    render();
+    rotate();
+  })
+  .catch((error) => console.error("Error loading world data: ", error));
