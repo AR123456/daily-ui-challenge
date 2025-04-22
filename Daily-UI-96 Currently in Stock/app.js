ceece67f5782b6547a0ad5dd517e5f7fa620dfe2
AR123456 <coloradoz99@gmail.com>
2025-04-22 05:04:17 -0400

    better js that works

--- Daily-UI-96 Currently in Stock/app.js
+++ Daily-UI-96 Currently in Stock/app.js
@@ -1,23 +1,9 @@
-// https://www.w3schools.com/css/css3_variables_javascript.asp
+document.querySelectorAll("tr").forEach((row) => {
+  const stockEl = row.querySelector(".num-stock");
+  const gaugeEl = row.querySelector(".gauge .percentage");
 
-let r = document.querySelector(":root");
-const stock = document.querySelector(".num-stock").innerHTML;
-const percentage = document.querySelector(".percentage").clientWidth;
-const mask = document.querySelector(".mask").clientWidth;
-
-console.log(stock);
-console.log(percentage);
-console.log(mask);
-
-function gageVal_get() {
-  let rs = getComputedStyle(r);
-  // console.log(rs);
-  console.log(rs.getPropertyValue("--rotation").slice(0, 2));
-}
-// gageVal_get();
-function gageVal_set() {
-  let rs = getComputedStyle(r);
-  console.log(rs.getPropertyValue("--rotation").slice(0, 2));
-  r.style.setProperty("--rotation", "stock");
-}
-gageVal_set();
+  if (stockEl && gaugeEl) {
+    const stock = stockEl.textContent.trim();
+    gaugeEl.style.transform = `rotate(${stock}deg)`;
+  }
+});
