4fba3ff882ff993375bf233da62ff2ebaeaa456e
AR123456 <coloradoz99@gmail.com>
2025-05-24 08:44:34 -0400

    loop videos

--- Daily-UI-003-Landing-Page/app.js
+++ Daily-UI-003-Landing-Page/app.js
@@ -1,5 +1,18 @@
 const video = document.getElementById("theVideo");
 const button = document.getElementById("theBtn");
+const videoSources = [
+  "./newFLY.mp4",
+  "./TreesFlyover.mp4",
+  "./Boardwalk_Slowerish_3.mp4",
+];
+let currentVideoIndex = 0;
+
+video.addEventListener("ended", () => {
+  currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
+  video.scr = videoSources[currentVideoIndex];
+  video.load();
+  video.play();
+});
 
 const pauseFunction = () => {
   if (video.paused) {
