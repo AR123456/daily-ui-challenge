56fe5ec922451b94fb06e4bc793aa5025f45c5a9
AR123456 <coloradoz99@gmail.com>
2025-06-02 08:53:51 -0400

    updating css

--- W3-Carousel/app.js
+++ W3-Carousel/app.js
@@ -1,12 +1,12 @@
 let slideIndex = 1;
-
 showSlides(slideIndex);
 
-// next prev controls
+// Next/previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
-// thumb nail controls
+
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
@@ -15,14 +15,16 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (let i = 0; i < dots.length; i++) {
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
