d08ba39e62bf73b9ba6a48a1e8d92fa2f4ab4d8a
AR123456 <coloradoz99@gmail.com>
2025-06-05 06:12:12 -0400

    dynamic number generation

--- Slideshow-Gallery/app.js
+++ Slideshow-Gallery/app.js
@@ -12,21 +12,32 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("thumb");
+  let thumbs = document.getElementsByClassName("thumb");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) {
+  const totalSlides = slides.length;
+
+  if (n > totalSlides) {
     slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length;
+    slideIndex = totalSlides;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  for (i = 0; i < thumbs.length; i++) {
+    thumbs[i].className = thumbs[i].className.replace(" active", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+  thumbs[slideIndex - 1].className += " active";
+
+  // Set dynamic slide number
+  const numberText = slides[slideIndex - 1].querySelector(".numbertext");
+  numberText.textContent = `${slideIndex} / ${totalSlides}`;
+
+  // Set caption
+  captionText.innerHTML = thumbs[slideIndex - 1].alt;
 }
