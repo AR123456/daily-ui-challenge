96cbd2af03fdbc97755ccf261c1148fa17869a9d
AR123456 <coloradoz99@gmail.com>
2025-06-05 06:27:35 -0400

    slide show gallery improved

---
+++ Slide-show-Gallery/app.js
@@ -0,0 +1,43 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let thumbs = document.getElementsByClassName("thumb");
+  let captionText = document.getElementById("caption");
+  const totalSlides = slides.length;
+
+  if (n > totalSlides) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = totalSlides;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < thumbs.length; i++) {
+    thumbs[i].className = thumbs[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  thumbs[slideIndex - 1].className += " active";
+
+  // Set dynamic slide number
+  const numberText = slides[slideIndex - 1].querySelector(".numbertext");
+  numberText.textContent = `${slideIndex} / ${totalSlides}`;
+
+  // Set caption
+  captionText.innerHTML = thumbs[slideIndex - 1].alt;
+}
