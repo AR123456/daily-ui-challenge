bbadcebe36738c54aa2da66ea9453bc5d851e400
AR123456 <coloradoz99@gmail.com>
2025-06-06 06:27:13 -0400

    working thumbs

--- Slide-show-Gallery/app.js
+++ Slide-show-Gallery/app.js
@@ -1,69 +1,18 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// document.querySelector(".prev").addEventListener("click", () => {
-//   plusSlides(-1);
-// });
-// document.querySelector(".next").addEventListener("click", () => {
-//   plusSlides(1);
-// });
-// const thumbs = document.querySelector(".thumb");
-// thumbs.forEach((thumb, index) => {
-//   thumb.addEventListener("click", () => {
-//     currentSlide(index + 1);
-//   });
-// });
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let thumbs = document.getElementsByClassName("thumb");
-//   let captionText = document.getElementById("caption");
-//   const totalSlides = slides.length;
-
-//   if (n > totalSlides) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = totalSlides;
-//   }
-
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-
-//   for (i = 0; i < thumbs.length; i++) {
-//     thumbs[i].className = thumbs[i].className.replace(" active", "");
-//   }
-
-//   slides[slideIndex - 1].style.display = "block";
-//   thumbs[slideIndex - 1].className += " active";
-
-//   // Set dynamic slide number
-//   const numberText = slides[slideIndex - 1].querySelector(".numbertext");
-//   numberText.textContent = `${slideIndex} / ${totalSlides}`;
-
-//   // Set caption
-//   captionText.innerHTML = thumbs[slideIndex - 1].alt;
-// }
 let slideIndex = 1;
 showSlides(slideIndex);
 
-document.querySelector(".prev").addEventListener("click", () => plusSlides(-1));
-document.querySelector(".next").addEventListener("click", () => plusSlides(1));
-
+document.querySelector(".prev").addEventListener("click", () => {
+  plusSlides(-1);
+});
+document.querySelector(".next").addEventListener("click", () => {
+  plusSlides(1);
+});
 const thumbs = document.querySelectorAll(".thumb");
 thumbs.forEach((thumb, index) => {
-  thumb.addEventListener("click", () => currentSlide(index + 1));
+  thumb.addEventListener("click", () => {
+    currentSlide(index + 1);
+  });
 });
-
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
@@ -75,21 +24,32 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("thumb");
+  let thumbs = document.getElementsByClassName("thumb");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) {
+  const totalSlides = slides.length;
+
+  if (n > totalSlides) {
     slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length;
+    slideIndex = totalSlides;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  for (i = 0; i < thumbs.length; i++) {
+    thumbs[i].className = thumbs[i].className.replace(" active", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+  thumbs[slideIndex - 1].className += " active";
+
+  // Set dynamic slide number
+  const numberText = slides[slideIndex - 1].querySelector(".numbertext");
+  numberText.textContent = `${slideIndex} / ${totalSlides}`;
+
+  // Set caption
+  captionText.innerHTML = thumbs[slideIndex - 1].alt;
 }
