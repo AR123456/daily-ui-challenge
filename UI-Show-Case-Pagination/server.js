2aa7c64e5cd6c192f4e49a237de420c4577c4aa7
AR123456 <coloradoz99@gmail.com>
2025-06-08 06:16:32 -0400

    create image array

--- UI-Show-Case-Pagination/server.js
+++ UI-Show-Case-Pagination/server.js
@@ -1,2 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+
+const folderPath = path.join(__dirname, "thumbs");
+const images = fs.readdirSync(folderPath).filter((file) => {
+  return /\.(jpe?g|png|gif|webp)$/i.test(file);
+});
+console.log(images);
+const imageArray = `const images=${JSON.stringify(images, null, 2)}`;
+fs.writeFileSync(path.join(__dirname));
